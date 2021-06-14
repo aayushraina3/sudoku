19ce7ffa52c03d005b987ad8e7580357ce6c8f8c
Aayush <aayushraina3@gmail.com>
2021-06-14 23:50:37 +0530

    added new board

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.use(express.static(public));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(public + '/index.html'));
-})
+});
+
+app.get('/boards/:board', (req, res)=>{
+    res.sendFile(path.join(path.join(public + '/boards') + `/${req.params.board}.html`));
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('Server set up.');
